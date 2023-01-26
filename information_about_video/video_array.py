1cddbbc3d3dfb0318844e787d1f608d89c394399
kir <konnov360@gmail.com>
2023-01-26 20:57:57 +0300

    массив с видео с ютуба

---
+++ information_about_video/video_array.py
@@ -0,0 +1,12 @@
+video = ['https://www.youtube.com/watch?v=fAl0hYvdP8g',
+         'https://www.youtube.com/watch?v=oYz-mj4Ax8w',
+         'https://www.youtube.com/watch?v=OtTOqWhaf9I',
+         'https://www.youtube.com/watch?v=Wi4_8stJAAg',
+         'https://www.youtube.com/watch?v=van-K3uZKpI',
+         'https://www.youtube.com/watch?v=_iHkGCDx56Y',
+         'https://www.youtube.com/watch?v=p1C6Do2_q08',
+         'https://www.youtube.com/watch?v=_iHkGCDx56Y',
+         'https://www.youtube.com/watch?v=mImfshRZgbA',
+         'https://www.youtube.com/watch?v=8AX_1Cl4BzY'
+         ]
+
