5a3428053df4a3f2cbc8311c5d2dd0e56488a844
kir <konnov360@gmail.com>
2023-02-09 23:46:41 +0300

    добавлен экз класса Video и HtmlText

--- bot/bot.py
+++ bot/bot.py
@@ -1,26 +1,29 @@
 from aiogram import Bot, Dispatcher, types, executor
 from aiogram.dispatcher.filters import Text
-from import_of_message.message_answer import *
-from keyboards import *
+from import_of_message.message_answer import HtmlText
+from keyboards import Keyboard
 from config import TOKEN
-from information_about_video.video_array import *
+from information_about_video.video_array import Video
 import random
-from scripts_database import *
+from scripts_database import Database
 
 # Объект бота
 bot = Bot(TOKEN)  # экземпляр бота , connection API
 dp = Dispatcher(bot)
-
+kb = Keyboard()
+db = Database()
+video = Video()
+htm = HtmlText()
 
 async def start_up_bot(_):
     print('Бот начал свою работу и готов к использованию  ')
 
 
 async def random_video(message: types.Message):
-    random_video = random.choice(video)
+    random_video = random.choice(video.array_video())
     await bot.send_message(chat_id=message.chat.id,
                            text=random_video,
-                           reply_markup=ikb
+                           reply_markup=kb.inline_buttons()
                            )
 
 
@@ -30,9 +33,9 @@ async def bot_launch(message: types.Message):
        Функции запуска бота в чат с ботом вводиться /start
     после чего бот приветствует пользователя по его id юзера
     message - это сообзение которое будет возвращенно пользователю '''
-    await message.answer(text=f'{HI} {message.from_user.first_name} {HI2}',
+    await message.answer(text=f'{htm.user_greeting()} {message.from_user.first_name} {htm.user_greeting_from_user_id()}',
                          parse_mode='HTML',
-                         reply_markup=key_board
+                         reply_markup=kb.menu_on_start_bot()
                          )  # приветствие пользователя
     await bot.send_photo(message.from_user.id,
                          photo='https://im.kommersant.ru/gboxtexts/sp_com/logos/005.jpg')
@@ -47,8 +50,8 @@ async def desc_command(message: types.Message):
     :param message:сообщение которое выводиться
     возвращается полная справка по боту
     '''
-    await message.answer(text=HELP, parse_mode='HTML',
-                         reply_markup=key_board_help
+    await message.answer(text=htm.return_help_info(), parse_mode='HTML',
+                         reply_markup=kb.button_help_on_bot()
                          )
     await message.delete()
 
@@ -57,7 +60,7 @@ async def desc_command(message: types.Message):
 async def contact_info(messages: types.Message):
     '''Функция для просмотра контактной информации вводиться /contactinfo
     Возвращается к.и (сайты, ссылки на социальные сети,адрес и т.д) '''
-    await messages.answer(text=CONTACT_INFO, parse_mode='HTML', reply_markup=key_board_contact_info)
+    await messages.answer(text=htm.return_contact_info(), parse_mode='HTML', reply_markup=kb.button_contact_information())
     await messages.delete()
 
 
@@ -66,11 +69,11 @@ async def location_organization(message: types.Message):
     '''Функция возврата организации на карте вводиться /location
     Возвращается информация на гугл картах которая содержит в себе кординаты и адрес организации
     " Северная железная дорога " '''
-    await message.answer(text=LOCATE, parse_mode='HTML')
+    await message.answer(text=htm.return_location(), parse_mode='HTML')
     await bot.send_location(chat_id=message.from_user.id,
                             latitude=57.636032,
                             longitude=39.890337,
-                            reply_markup=key_board
+                            reply_markup=kb.button_geopozition()
                             )
     # кординаты организации 57.636032, 39.890337 из гугл карт
     await message.delete()
@@ -79,14 +82,14 @@ async def location_organization(message: types.Message):
 @dp.message_handler(Text(equals='🚂Назад🚂'))
 async def back_to_menu(message: types.Message):
     await message.answer(text='Вы вернулись на раздел назад',
-                         reply_markup=key_board_of_menu_support)
+                         reply_markup=kb.menu_support())
     await message.delete()
 
 
 @dp.message_handler(Text(equals='🚂Главное меню для сотрудников🚂'))
 async def menu_of_support(message: types.Message):
     await message.answer(text='Вы перешли в главное меню',
-                         reply_markup=key_board_of_menu_support)
+                         reply_markup=kb.menu_support())
     await message.delete()
 
 
@@ -94,7 +97,7 @@ async def menu_of_support(message: types.Message):
     Text(equals='🚂Полезные видео для специалистов технической поддержки🚂'))  # дописать кнопку со статьями
 async def article_support(message: types.Message):
     await message.answer(text='Вы перешли в этот раздел для просмотра статей',
-                         reply_markup=key_board_articles_support)
+                         reply_markup=kb.button_random_video())
     await message.delete()
 
 
@@ -106,77 +109,77 @@ async def send_random_article(message: types.Message):
 @dp.message_handler(Text(equals='🚂Местоположение организации🚂'))
 async def recieve_location_organization(message: types.Message):
     await message.answer(text='Вы перешли в этот раздел для просмотра местоположения организации',
-                         reply_markup=key_board_geo_position)
+                         reply_markup=kb.button_geopozition())
     await message.delete()
 
 
 @dp.message_handler(Text(equals='🚂Контактная информация🚂'))
 async def recieve_contact_info_organization(message: types.Message):
     await message.answer(text='Вы перешли в этот раздел для просмотра контанктной информации организации',
-                         reply_markup=key_board_contact_info)
+                         reply_markup=kb.button_contact_information())
     await message.delete()
 
 
 @dp.message_handler(Text(equals='🚂Вспомогательная информации по использованию бота🚂'))
 async def help_information_bot(message: types.Message):
     await message.answer(text='Вы перешли в раздел вспомогательной информации по боту @RZD_TECHNICAL_BOT',
-                         reply_markup=key_board_help)
+                         reply_markup=kb.button_help_on_bot())
     await message.delete()
 
 
 @dp.message_handler(Text(equals='🚂Вернуться в Стартовое меню🚂'))
 async def help_information_bot(message: types.Message):
     await message.answer(text='Вы перешли в раздел вспомогательной информации по боту @RZD_TECHNICAL_BOT',
-                         reply_markup=key_board)
+                         reply_markup=kb.menu_on_start_bot())
     await message.delete()
 
 
 @dp.message_handler(Text(equals='🚂Вспомогательная информации по использованию бота🚂'))
 async def help_info_of_support(message: types.Message):
     await message.answer(text='Вы перешли в раздел вспомогательной ифнормации',
-                         reply_markup=key_board_help)
+                         reply_markup=kb.button_help_on_bot())
     await message.delete()
 
 
 @dp.message_handler(Text(equals='🚂Для сотрудников технической поддержки🚂'))
 async def request_bd(message: types.Message):
     await message.answer(text='Вы перешли в раздел для специалистов тех поддержки',
-                         reply_markup=key_board_bd)
+                         reply_markup=kb.key_board_bd())
     await message.delete()
 
 
 @dp.message_handler(Text(equals='🚂Запросы на вывод🚂'))
 async def request_bd(message: types.Message):
     await message.answer(text='Вы перешли в раздел сформированных запросов БД',
-                         reply_markup=key_board_menu_support)
+                         reply_markup=kb.button_select_info())
     await message.delete()
 
 
 @dp.message_handler(Text(equals='🚂Вернуться назад🚂'))
 async def back_menu(message: types.Message):
     await message.answer(text='Вы вернулись назад',
-                         reply_markup=key_board_bd)
+                         reply_markup=kb.menu_support())
     await message.delete()
 
 
 @dp.message_handler(Text(equals='🚂Вывести всех сотрудников с их личной информацией🚂'))
 async def conclusion_employees(message: types.Message):
-    await message.answer(text=convert_to_string_employees(all_employees))
+    await message.answer(text=db.convert_to_string_employees())
     await message.delete()
 
 @dp.message_handler(Text(equals='🚂Вывести имя, фамилию и справку которую заказал сотрудник🚂'))
 async def consolution_email_name(message:types.Message):
-    await message.answer(text=convert_inf_employees(all_email_name))
+    await message.answer(text=db.convert_inf_employees())
     await message.delete()
 
 @dp.message_handler(Text(equals='🚂Вывести все виды справок🚂'))
 async def consolution_information(message:types.Message):
-    await message.answer(text=convert_information(all_information))
+    await message.answer(text=db.convert_information())
     await message.delete()
 
 @dp.message_handler(Text(equals='🚂Вывести все почты сотрудников🚂'))
 async def consolution_email(message:types.Message):
-    await message.answer(text=convert_email(all_email))
+    await message.answer(text=db.convert_email())
 
 flag = False
 @dp.callback_query_handler()
