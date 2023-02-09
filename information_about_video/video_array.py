302522e9184ecf74441c98a735cf5c98e32da27e
kir <konnov360@gmail.com>
2023-02-09 23:47:58 +0300

    добавлен класс Video

--- information_about_video/video_array.py
+++ information_about_video/video_array.py
@@ -1,12 +1,22 @@
-video = ['https://www.youtube.com/watch?v=fAl0hYvdP8g',
-         'https://www.youtube.com/watch?v=oYz-mj4Ax8w',
-         'https://www.youtube.com/watch?v=OtTOqWhaf9I',
-         'https://www.youtube.com/watch?v=Wi4_8stJAAg',
-         'https://www.youtube.com/watch?v=van-K3uZKpI',
-         'https://www.youtube.com/watch?v=_iHkGCDx56Y',
-         'https://www.youtube.com/watch?v=p1C6Do2_q08',
-         'https://www.youtube.com/watch?v=_iHkGCDx56Y',
-         'https://www.youtube.com/watch?v=mImfshRZgbA',
-         'https://www.youtube.com/watch?v=8AX_1Cl4BzY'
-         ]
+class Video:
+    video = ['https://www.youtube.com/watch?v=fAl0hYvdP8g',
+             'https://www.youtube.com/watch?v=oYz-mj4Ax8w',
+             'https://www.youtube.com/watch?v=OtTOqWhaf9I',
+             'https://www.youtube.com/watch?v=Wi4_8stJAAg',
+             'https://www.youtube.com/watch?v=van-K3uZKpI',
+             'https://www.youtube.com/watch?v=_iHkGCDx56Y',
+             'https://www.youtube.com/watch?v=p1C6Do2_q08',
+             'https://www.youtube.com/watch?v=_iHkGCDx56Y',
+             'https://www.youtube.com/watch?v=mImfshRZgbA',
+             'https://www.youtube.com/watch?v=8AX_1Cl4BzY'
+             ]
 
+    @classmethod
+    def vid(cls):
+        return cls.video
+
+    def __init__(self):
+        pass
+
+    def array_video(self):
+        return self.vid()
